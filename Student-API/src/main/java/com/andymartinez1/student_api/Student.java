3a849b339d52dd6623d9e76affef3f315905bbd9
Andrew Martinez <andy72391@gmail.com>
2024-08-15 13:51:57 -0400

    Building a student database API using Springboot.

---
+++ Student-API/src/main/java/com/andymartinez1/student_api/Student.java
@@ -0,0 +1,61 @@
+package com.andymartinez1.student_api;
+
+import java.util.Objects;
+
+// import java.util.Objects;
+
+public class Student {
+    private Integer id;
+    private String name;
+
+    public Student() {
+    }
+
+    public Student(String name) {
+        super();
+        this.name = name;
+    }
+
+    public Student(Integer id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Student other = (Student) obj;
+        return Objects.equals(id, other.id);
+    }
+
+}
---
+++ Student-API/src/main/java/com/andymartinez1/student_api/StudentApiController.java
@@ -0,0 +1,53 @@
+package com.andymartinez1.student_api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentApiController {
+    private static List<Student> listStudents = new ArrayList<>();
+    private static Integer studentId = 0;
+
+    static {
+        listStudents.add(new Student(studentId++, "George"));
+        listStudents.add(new Student(studentId++, "Johnny"));
+    }
+
+    @GetMapping
+    public ResponseEntity<?> list() {
+        if (listStudents.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return new ResponseEntity<>(listStudents, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> add(@RequestBody Student student) {
+        student.setId(studentId++);
+        listStudents.add(student);
+
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> replace(@RequestBody Student student) {
+        if (listStudents.contains(student)) {
+            int index = listStudents.indexOf(student);
+            listStudents.set(index, student);
+
+            return new ResponseEntity<>(student, HttpStatus.OK);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
